26935ae8fedbafda573fb3696be5b46832ef7ed4
EC2 Default User <ec2-user@ip-172-31-33-58.eu-north-1.compute.internal>
2024-11-22 09:59:08 +0000

    管理者ログイン

--- app/controllers/admin/admins/sessions_controller.rb
+++ app/controllers/admin/admins/sessions_controller.rb
@@ -18,8 +18,11 @@ class Admin::Admins::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
+    def after_sign_in_path_for(resource)
+      admin_users_path  # 管理者の会員一覧ページ
+    end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
