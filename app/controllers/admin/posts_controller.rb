40eb2c7e49deedc2ace5a52ff454f6872b8aeec4
EC2 Default User <ec2-user@ip-172-31-33-58.eu-north-1.compute.internal>
2024-11-23 07:16:03 +0000

    投稿一覧、詳細作成

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,8 +1,14 @@
 class Admin::PostsController < ApplicationController
-  before_action :authenticate_admin!
+  before_action :authenticate_admin! # 管理者認証を適用
 
   def index
     @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @user = @post.user # 投稿者情報を取得
+    @comments = @post.post_comments.includes(:user) # コメントも取得
   end
 
   def destroy
